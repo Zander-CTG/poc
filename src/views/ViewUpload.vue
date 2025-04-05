a9c37d184c42b6d621f9776dea3f896d601d2a07
Michael Joy <11379243+michael-255@users.noreply.github.com>
2025-04-05 12:13:04 -0400

    initial text for what is uploaded (#7)

--- src/views/ViewUpload.vue
+++ src/views/ViewUpload.vue
@@ -199,7 +199,15 @@ function processResponseItems(
         <q-item-section top>
           <q-item-label>Image Uploader</q-item-label>
           <q-item-label caption>
-            Choose an image to upload and have processed.
+            Add items by choosing an image to upload and have processed. The
+            following fields will be populated based on the image content:
+            <ul>
+              <li>Type</li>
+              <li>Brand</li>
+              <li>Label</li>
+              <li>Description</li>
+              <li>Categories</li>
+            </ul>
           </q-item-label>
         </q-item-section>
       </q-item>
